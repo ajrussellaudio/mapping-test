c794af65a114ab31a94c27de2bfa74cb7e12fe3e
Alan Russell <ajrussellaudio@gmail.com>
2023-12-13 19:23:21 +0000

    Install Vitest and React Testing Library

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import cesium from "vite-plugin-cesium";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), cesium()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./tests/setup.ts"],
+  },
 });
